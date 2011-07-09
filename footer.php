123983381fbcb659665cd0eaa9169dd85d39fe2f
enile8 <email@enile8.org>
2011-07-09 00:46:18 -0400

    first commit

---
+++ footer.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the id=main div and all content after
+ *
+ * @package WordPress
+ * @subpackage Twenty_Eleven
+ * @since Twenty Eleven 1.0
+ */
+?>
+
+	</div><!-- #main -->
+
+	<footer id="colophon" role="contentinfo">
+
+			<?php
+				/* A sidebar in the footer? Yep. You can can customize
+				 * your footer with three columns of widgets.
+				 */
+				get_sidebar( 'footer' );
+			?>
+
+			
+			
+			<div id="site-generator">
+			
+			
+				<?php do_action( 'twentyeleven_credits' ); ?>
+				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentyeleven' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentyeleven' ); ?>" rel="generator">WordPress Powered</a> <br/>
+				<a href="http://enile8.org" title="Twenty Eleven Child Theme">Theme by enile8</a>
+			</div>
+			
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
